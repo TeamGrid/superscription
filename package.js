5a9c62433a1dde44425768e0bfbfb668bc16662f
Max Nowack <max@unsou.de>
2016-05-25 14:00:43 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'teamgrid:superscription',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Sophisticated subscription handling for meteor',
   // URL to the Git repository containing the source code for this package.
