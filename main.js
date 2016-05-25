149968fa1222b589b898af3b9f1b725cc8920952
Max Nowack <max@unsou.de>
2016-05-25 13:59:05 +0200

    fix allReady callbacks

--- main.js
+++ main.js
@@ -22,44 +22,51 @@ export class Superscription {
     this._allReadyCallbacks = []
     this._readyAutorunCallbacks = []
     this._isReady = new ReactiveVar(false)
+    this._areAllReady = new ReactiveVar(false)
     this._subs = new ReactiveVar({})
 
     Tracker.autorun(() => {
       if (this.subscription.ready()) {
-        this._readyCallbacks.forEach((fn) => fn.call(this))
+        this._readyCallbacks.forEach((fn) => Tracker.nonreactive(() => fn.call(this)))
         this._readyAutorunCallbacks.forEach((fn) => Tracker.autorun((computation) => {
           const sub = fn.call(this, this._registerSubscription.bind(this), computation)
-          if (sub && sub.subscriptionId) this._registerSubscription(sub)
+          if (sub && sub.subscriptionId) this._registerSubscription(sub, computation)
         }))
       }
       this._isReady.set(this.subscription.ready())
     })
 
     Tracker.autorun(() => {
+      this._areAllReady.set(this.isReady() && _.every(_.values(this._subs.get())))
+    })
+
+    Tracker.autorun(() => {
       if (this.areAllReady()) {
-        this._allReadyCallbacks.forEach((fn) => fn.call(this))
+        this._allReadyCallbacks.forEach((fn) => Tracker.nonreactive(() => fn.call(this)))
       }
     })
   }
 
-  _registerSubscription(sub) {
-    if (Tracker.active) {
-      Tracker.onInvalidate(() => {
+  _registerSubscription(sub, computation) {
+    if (computation) {
+      computation.onInvalidate(() => {
         const current = this._subs.get()
-        delete current[sub.subscriptionId]
-        this._subs.set(current)
+        if (!current[sub.subscriptionId]) {
+          delete current[sub.subscriptionId]
+          this._subs.set(current)
+        }
       })
     }
     const current = Tracker.nonreactive(() => this._subs.get())
     const ready = sub.areAllReady ? sub.areAllReady() : sub.ready()
-    if (this._subs[sub.subscriptionId] !== ready) {
-      this._subs[sub.subscriptionId] = ready
+    if (current[sub.subscriptionId] !== ready) {
+      current[sub.subscriptionId] = ready
       this._subs.set(current)
     }
   }
 
   isReady() { return this._isReady.get() }
-  areAllReady() { return this.isReady() && _.every(_.values(this._subs.get())) }
+  areAllReady() { return this._areAllReady.get() }
 
   ready(fn) {
     this._readyCallbacks.push(fn)
